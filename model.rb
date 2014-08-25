9ed28e754dbf0aa7bde7801be43eac40e727d7ae
Taha Husain <taha.husain@weboniselab.com>
2014-08-25 20:39:51 +0530

    Finish: Configuration YAML file

--- model.rb
+++ model.rb
@@ -1,6 +1,7 @@
+#!/usr/bin/env ruby
 require 'yaml'
 def create_config_file
-  information = {"Development" => {"adapter" => "mysql2", "hostname" => "localhost", "username" => "root", "password" => ""}}
+  information = {"development" => {"adapter" => "mysql2", "hostname" => "localhost", "username" => "root", "password" => ""}}
   file = File.new("database.yml","w")
   top_string = "# MySQL version 5.x
 # 	gem install mysql2
@@ -8,40 +9,27 @@ def create_config_file
 #	This file contains database configuration for mysql2 gem
 #	To change the credentials or hostname, change it in here...
 #	This field is meant only for database\n"
+  
   bottom_string = "# You can add another yaml document below with a header and
 # then list of keys and values
 
 # Database:
 #  adapter: sqlite3
 #  database: db/development.sqlite3\n"
+  
   file.write(top_string + information.to_yaml + bottom_string)
   file.close
 end
 
 def read_config_file
   remove_comments = Array.new()
-  file = File.open("database.yml", "r")
-  lines = file.readlines
-  file.close
-  lines.each do |line|
-  	if line.start_with? "#"
-      remove_comments.push(line)
-    end
-  end
-  lines -= remove_comments
+  file = YAML.load_file("database.yml")
 end
 
 def extract_configuration
   lines = read_config_file
-  key, value = [], []
-  lines.each do |line|
-  	if line.include? ":"
-  	  unless line.split(":")[1].strip.empty?
-  	  	key.push(line.split(":")[0].strip)
-  	  	value.push(line.split(":")[1].strip)
-  	  end
-  	end
-  end
-  configurations = Hash[key.zip value]
-  puts configurations
+  puts lines["development"]
 end
+
+create_config_file
+extract_configuration
