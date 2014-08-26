c27d3459b1ae7adf3b412b8e8da657ef18c5ba9f
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-08-26 14:23:45 +0530

    modified

--- model.rb
+++ model.rb
@@ -3,5 +3,19 @@ class Model
   def self.generating_model
     GeneratingModel.get_model_parameter ARGV
   end
+  def self.get_paramenter
+    arguement_counter = 0
+    data_type = []
+    column_name = []
+    ARGV.each do |arguement|
+      if arguement_counter>=2
+        column = arguement.split(':')
+        data_type << column[0]
+        column_name << column[1]
+      end
+      arguement_counter += 1
+    end
+    return data_type,column_name
+  end
 end
 Model.generating_model
