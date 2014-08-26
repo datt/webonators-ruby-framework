9590b28698e6b7ce4ddc58fa1a4851066fb57803
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-08-26 15:59:53 +0530

    Changed

--- generating_model.rb
+++ generating_model.rb
@@ -11,36 +11,17 @@ module GeneratingModel
       model_name = model_name.downcase
       model_class_name = model_name.capitalize
       validate_flag = validate
-      write_model.write "class #{model_class_name} < WeboModel\nend"
+      write_model.write "class #{model_class_name} < WeboModel\n"
       write_model.close
       if validate_flag.eql?1
         data_type,column_name = get_model_attribute
+        set_column data_type,column_name,model_class_name
       elsif validate_flag.eql?0
         puts "Bad command..!!Try again"
       elsif validate_flag.eql?2
       end
       create_class model_class_name,column_name
-      get_paramenter
     end
-  end
-
-  def self.get_paramenter
-    arguement_counter = 0
-    data_type = ["int","string"]
-    column_name = ["name","age"]
-    ==begin
-    @arg.each do |arguement|
-      if arguement_counter>=2
-        column = arguement.split(':')
-        data_type << column[0]
-        column_name << column[1]
-      end
-      arguement_counter += 1
-    end
-    ==end
-    p data_type
-    p column_name
-    return data_type,column_name
   end
 
   def self.get_model_attribute
@@ -62,7 +43,7 @@ module GeneratingModel
     arguement_counter = 0
     validate_flag = 0
     arguement_datatype = []
-    data_type = ["int","tinyint","smallint","mediumint","begint","float","double","decimal","date","datetime","timestamp","time","year","char","varchar","blob","tinyblob","mediumblob","longblob","enum"]
+    data_type = ["Integer","Float","Boolean","String"]
     if @arg.size.eql?2
       validate_flag =2
       return validate_flag
@@ -80,6 +61,21 @@ module GeneratingModel
       arguement_counter += 1
     end
     return validate_flag
+  end
+
+  def self.set_column data_type,column_name,model_class_name
+    loop_counter = 0
+    write_file = File.open("#{@file_name}.rb","a+")
+    write_file.each_line do |line|
+      if line.scan"class #{model_class_name}"
+        while(loop_counter<=data_type.size-1)
+          write_file.write"\s\s#{column_name[loop_counter]}:#{data_type[loop_counter]}"
+          write_file.write"\nend"
+          loop_counter += 1
+        end
+      end
+    end
+    write_file.close
   end
 
   def self.method_name
