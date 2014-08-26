ffbe58659441e8a3f6de0e952f867e337c81dd40
Taha Husain <taha.husain@weboniselab.com>
2014-08-26 11:43:00 +0530

    Finish: Configuration YAML file

--- model.rb
+++ model.rb
@@ -1,34 +1,43 @@
 #!/usr/bin/env ruby
 require 'yaml'
-def create_config_file
-  information = {"development" => {"adapter" => "mysql2", "hostname" => "localhost", "username" => "root", "password" => ""}}
-  file = File.new("database.yml","w")
-  top_string = "# MySQL version 5.x
-# 	gem install mysql2
-#
-#	This file contains database configuration for mysql2 gem
-#	To change the credentials or hostname, change it in here...
-#	This field is meant only for database\n"
+
+module GenerateConfigurationFile 
+  def create_config_file
+    INFORMATION = { "development" => 
+      {"adapter" => "mysql2", 
+       "database" => "test",
+       "hostname" => "localhost",
+       "username" => "root",
+       "password" => ""}
+      }
+    file = File.new("database.yml","w")
+    TOP_STRING = "# MySQL version 5.x
+					# 	gem install mysql2
+					#
+					#	This file contains database configuration for mysql2 gem
+					#	To change the credentials or hostname, change it in here...
+					#	This field is meant only for database\n"
   
-  bottom_string = "# You can add another yaml document below with a header and
-# then list of keys and values
+  	BOTTOM_STRING = "# You can add another yaml document below with a header and
+					# then list of keys and values
 
-# Database:
-#  adapter: sqlite3
-#  database: db/development.sqlite3\n"
+					# Database:
+					#  adapter: sqlite3
+					#  database: db/development.sqlite3\n"
   
-  file.write(top_string + information.to_yaml + bottom_string)
-  file.close
-end
+    file.write(TOP_STRING + INFORMATION.to_yaml + BOTTOM_STRING)
+    file.close
+  end
 
-def read_config_file
-  remove_comments = Array.new()
-  file = YAML.load_file("database.yml")
-end
+  def read_config_file
+    remove_comments = Array.new()
+    file = YAML.load_file("database.yml")
+  end
 
-def extract_configuration
-  configuration = read_config_file
-  puts configuration["development"]
+  def extract_configuration
+    configuration = read_config_file
+    puts configuration["development"]
+  end
 end
 
 create_config_file
