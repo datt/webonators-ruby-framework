af7a42e79690ada74f52b89b12000a11edbd738f
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-08-27 17:59:22 +0530

    changes

--- generating_model.rb
+++ generating_model.rb
@@ -2,34 +2,56 @@ require_relative "webomodel.rb"
 module GeneratingModel
 
  def self.get_model_parameter argv
-    @arg = argv
-    @file_name = method_name
-    if @file_name.eql? "empty"
-      puts "File can't be created"
-    else
-      write_model = File.new("#{@file_name}.rb","a")
-      model_name = @arg[1]
-      model_name = model_name.downcase
-      model_class_name = model_name.capitalize
-      validate_flag = validate
-      write_model.write "class #{model_class_name} < WeboModel\n"
-      write_model.close
-      if validate_flag.eql?1
-        data_type,column_name = get_model_attribute
-        set_column data_type,column_name,model_class_name
-      elsif validate_flag.eql?0
-        puts "Bad command..!!Try again"
-      elsif validate_flag.eql?2
+    #@arg = argv
+
+     # write_model = File.new("#{@file_name}.rb","a")
+      puts argv[1]
+      if argv[1].eql?"model"
+        puts "i m model"
+        @file_name = method_name argv
+        if @file_name.eql? "empty"
+          puts "File can't be created"
+        else
+          model_name = argv[2]
+          if model_name[model_name.length-1].eql?'s'
+            model_name[model_name.length-1] = ''
+          end
+          model_name = model_name.downcase
+          model_class_name = model_name.capitalize
+          validate_flag = validate argv
+          write_model.write "class #{model_class_name} < WeboModel\n"
+          write_model.close
+          if validate_flag.eql?1
+            data_type,column_name = get_model_attribute argv
+            set_column data_type,column_name,model_class_name
+          elsif validate_flag.eql?0
+           puts "Bad command..!!Try again"
+          end
+        end
+      elsif argv[1].eql?"controller"
+        puts "here in controller"
+        controller_name = argv[2]
+        if controller_name[controller_name.length-1]!='s'
+          controller_name[controller_name.length]='s'
+        end
+        controller_name = controller_name.downcase
+        p controller_name
+        controller_class_name = controller_name.capitalize
+        p controller_class_name
+        write_controller = File.new("#{controller_name}_controller.rb","w")
+        write_controller.write "class #{controller_class_name}Controller < WeboController\n"
+        write_controller.close
+        command_length=argv.length
       end
     end
   end
 
-  def self.get_model_attribute
+  def self.get_model_attribute argv
     arguement_counter = 0
     data_type = []
     column_name = []
-    @arg.each do |arguement|
-      if arguement_counter>=2
+    argv.each do |arguement|
+      if arguement_counter>=3
         column = arguement.split(':')
         data_type << column[0]
         column_name << column[1]
@@ -39,24 +61,23 @@ module GeneratingModel
     return data_type,column_name
   end
 
-  def self.validate
+  def self.validate argv
     arguement_counter = 0
     validate_flag = 0
     arguement_datatype = []
     data_type = ["Integer","Float","Boolean","String"]
-    if @arg.size.eql?2
-      validate_flag =2
-      return validate_flag
-    end
-    @arg.each do |arguement|
-      if arguement_counter>=2
-      arguement_datatype = arguement.split(':')
+    argv.each do |arguement|
+      if arguement_counter>=3
+        arguement_datatype = arguement.split(':')
         if data_type.include?(arguement_datatype[0])
           validate_flag = 1
         else
           validate_flag = 0
           return validate_flag
         end
+      end
+      if argv.length == 3
+        validate_flag = 1
       end
       arguement_counter += 1
     end
@@ -79,12 +100,12 @@ module GeneratingModel
     write_file.close
   end
 
-  def self.method_name
+  def self.method_name argv
     create_model_flag = 0
     @file_name = "empty"
-    if @arg[0].eql?("new")
-      unless @arg[1].eql?("")
-        @file_name = @arg[1].downcase
+    if argv[0].eql?("new")
+      unless argv[2].eql?("")
+        @file_name = argv[2].downcase
         create_file = File.new("#{@file_name}.rb","w")
         create_file.close
       end
@@ -97,7 +118,6 @@ module GeneratingModel
   end
 
   def self.create_class model_class_name,column_name
-    puts "In create class"
     klass = Class.new WeboModel
     klass.class_eval do
       attr_accessor *column_name
--- model.rb
+++ model.rb
@@ -1,11 +1,12 @@
 require_relative "generating_model.rb"
 class Model
   def self.generating_model
-   argv = ["new","post","String:name"]
+   argv = ["new","controller","post"]
    GeneratingModel.get_model_parameter argv
-   model_class_name = argv[1].capitalize
+   model_class_name = argv[2].capitalize
    model_class_name.capitalize
-   data_type, column_name = GeneratingModel.get_model_attribute
+   data_type, column_name = GeneratingModel.get_model_attribute argv
+   p column_name
    GeneratingModel.create_class model_class_name,column_name
   end
 end
