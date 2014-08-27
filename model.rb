d2763d5fbb8b97411b4570045ab165403b5c4f31
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-08-27 14:18:36 +0530

    Dynamic Inheritance in Model done

--- model.rb
+++ model.rb
@@ -1,8 +1,12 @@
 require_relative "generating_model.rb"
 class Model
   def self.generating_model
-   GeneratingModel.get_model_parameter ARGV
+   argv = ["new","post","String:name"]
+   GeneratingModel.get_model_parameter argv
+   model_class_name = argv[1].capitalize
+   model_class_name.capitalize
+   data_type, column_name = GeneratingModel.get_model_attribute argv
+   GeneratingModel.create_class model_class_name,column_name
   end
 end
-
 Model.generating_model
