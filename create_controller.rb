7c2f60274bba3c8f8dd43ac1d044bf0343d7ba5f
kartik-webonise <kartik.nagre@weboniselab.com>
2014-08-27 12:35:51 +0530

    pusj=hing without meta programming

--- create_controller.rb
+++ create_controller.rb
@@ -10,69 +10,66 @@ module CreateController
       if controller_name[controller_name.length-1]!='s'
         controller_name[controller_name.length]='s'
       end
-
-      puts controller_name
-      create_file_in_controller controller_name
-      create_folder_in_view controller_name
-      write_to_file_in_controller_without_actions controller_name
-
+      write_to_file_in_controller_without_actions1 controller_name
       command_length=ARGV.length
       if command_length > 3
-        #write_to_file_in_controller_without_actions controller_name
         for  i in 3...command_length
           action=ARGV[i].downcase
+          create_file_in_controller controller_name
+          create_folder_in_view controller_name
           create_files_in_view controller_name,action
           write_action_in_controller controller_name,action
           write_in_routes controller_name,action
         end
+        write_end controller_name
+      elsif command_length <=3
+        create_file_in_controller controller_name
+        create_folder_in_view controller_name
+        write_to_file_in_controller_without_actions controller_name
       end
     end
   end
 
   def self.create_file_in_controller(controller_name)
-    puts "in 2nd method"
-    puts controller_name
-    File.new("app/controllers/#{controller_name}_controller.rb","w")
+    File.new("app/controllers/#{controller_name}_controller.rb","a")
   end
 
   def self.create_folder_in_view(controller_name)
-    puts "in 3rd method"
     path = "app/views/#{controller_name}"
     FileUtils.mkdir_p(path) unless File.exists?(path)
   end
 
   def self.create_files_in_view(controller_name,action)
-    #puts action
     File.new("app/views/#{controller_name}/#{action}.html.erb","w")
   end
 
-
   def self.write_action_in_controller(controller_name,action)
-
-    file=File.open("app/controllers/#{controller_name}_controller.rb", "r+")
+    file=File.open("app/controllers/#{controller_name}_controller.rb", "a")
     controller_name_class = controller_name.capitalize
-    if  file.readline("class #{controller_name} < ActionController::Base")
-      file.write("\n\s\sdef\s#{action}\n\s\send")
-    end
-    #file.write("\nend")
+    puts "--------------#{controller_name_class}--------------"
+    file.write("\n\s\sdef\s#{action}\n\s\send")
     file.close
   end
 
-=begin
-  def self.write_to_file_in_controller_with_actions(controller_name)
-    controller_name_class = controller_name.capitalize
-    file=File.open("app/controllers/#{controller_name}_controller.rb", "w")
-    file.write("class #{controller_name_class} < ActionController::Base")
-    file.write("\nend")
+  def self.write_end(controller_name)
+    file=File.open("app/controllers/#{controller_name}_controller.rb", "a")
+    file.write("\nend\n")
     file.close
   end
-=end
+
 
   def self.write_to_file_in_controller_without_actions(controller_name)
     controller_name_class = controller_name.capitalize
     file=File.open("app/controllers/#{controller_name}_controller.rb", "w")
     file.write("class #{controller_name_class} < ActionController::Base")
-    #file.write("\nend")
+    file.write("\nend\n")
+    file.close
+  end
+
+  def self.write_to_file_in_controller_without_actions1(controller_name)
+    controller_name_class = controller_name.capitalize
+    file=File.open("app/controllers/#{controller_name}_controller.rb", "a")
+    file.write("class #{controller_name_class} < ActionController::Base")
     file.close
   end
 
