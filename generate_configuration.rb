93d802b216ec60a101ecf3a316d0482de6df7911
minakshi-webonise <minakshi.najardhane@weboniselab.com>
2014-09-01 14:35:28 +0530

    removed unnecessory methods from generate_configuration

--- generate_configuration.rb
+++ generate_configuration.rb
@@ -9,16 +9,6 @@ module GenerateConfigurationFile
     file.close
   end
 
-  def self.read
-    remove_comments = []
-    file = YAML.load_file("database.yml")
-  end
-
-  def self.extract
-    configuration = read_config_file
-    configuration["development"]
-  end
-
   def self.create_routes_file
     file = File.new("routes.rb", "w")
     file.write(Constants::ROUTES_STRING)
