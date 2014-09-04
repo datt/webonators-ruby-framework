a373dd132f6b0e59aecee3f78fc3940dc0f105ab
minakshi-webonise <minakshi.najardhane@weboniselab.com>
2014-09-04 18:00:46 +0530

    removed unnecessory files

--- generator_model_controller.rb
+++
@@ -1,6 +0,0 @@
-require ::File.expand_path("../../support/execute_generator.rb", __FILE__)
-class Generator
-  def self.generating_model_controller argv
-     ExecuteGenerator.get_generator_parameter argv
-  end
-end
