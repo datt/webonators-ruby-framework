65662bf59a57b5cbeeb95c747711cfdb2f35e7f7
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-09-07 13:07:48 +0530

    changes in generator

--- sample/config/application.rb
+++
@@ -1,8 +0,0 @@
-require 'webit'
-require ::File.expand_path('../routes.rb', __FILE__)
-Dir["app/controllers/*.rb"].each {|file| require_relative "../"+file}
-Dir["app/models/*.rb"].each {|file| require_relative "../"+file}
-module Sample
-  class Application < Request
-  end
-end
