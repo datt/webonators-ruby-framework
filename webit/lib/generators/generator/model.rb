cbd591ae4423fc7b9057bedb2b89afdb8004b115
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-09-08 12:13:33 +0530

    major changes in generator

--- webit/lib/generators/generator/model.rb
+++ webit/lib/generators/generator/model.rb
@@ -3,38 +3,46 @@ require ::File.expand_path("../../generator.rb", __FILE__)
 require 'fileutils'
 
 class Model
-  VALID = true
-  INVALID = false
 
+  DATA_TYPE = ["integer","float","boolean","string","text"]
+  MIN_ARGUMENT_LENGTH = 3
   def call_for_model_operations argv
-    file_name = method_name argv
+    model_name = argv[2]
+    file_name = get_file_name argv
     if file_name.nil?
       abort "File can't be created"
     else
-      write_model_file file_name
-      model_name = argv[2]
-      if model_name[model_name.length-1].eql?'s'
-        model_name[model_name.length-1] = ''
-      end
-      model_name = model_name.downcase
+      create_model_file file_name
+      model_name = singularize model_name
       model_class_name = model_name.capitalize
       validate_flag = validate argv
-      write_model = File.open("app/models/#{model_name}.rb","a")
-      write_model.write "class #{model_class_name} < WebitModel\n"
-      write_model.close
+      write_in_model_file model_name,model_class_name
       if validate_flag
         data_type,column_name = get_model_attribute argv
-        set_column data_type,column_name,model_class_name,argv
+        set_column data_type,column_name,model_class_name,file_name
         WebitModel.create_table argv[2],data_type,column_name
-      elsif validate_flag.eql? INVALID
-        puts "Wrong Syntax..Command to generate model => webit g model mode_name data_type:column_name"
+      else
+        abort "Wrong Syntax..Command to generate model => webit g model mode_name data_type:column_name"
       end
     end
   end
 
   private
 
-    def write_model_file file_name
+    def singularize model_name
+      if model_name[model_name.length-1].eql?'s'
+        model_name[model_name.length-1] = ''
+      end
+      model_name
+    end
+
+    def write_in_model_file model_name,model_class_name
+      write_model = File.open("app/models/#{model_name}.rb","a")
+      write_model.write "class #{model_class_name} < WebitModel\n"
+      write_model.close
+    end
+
+    def create_model_file file_name
       File.exists?("app/models/#{file_name}.rb")
       create_file = File.new("app/models/#{file_name}.rb","w")
       create_file.close
@@ -44,42 +52,37 @@ class Model
       arguement_counter = 0
       data_type = []
       column_name = []
-      argv.each do |arguement|
-        if arguement_counter >= 3
-          column = arguement.split(':')
+      if argv.length > MIN_ARGUMENT_LENGTH
+        data_type_column_name_array = argv.drop(3)
+        data_type_column_name_array.each do |element|
+          column = element.split(':')
           data_type << column[0]
           column_name << column[1]
         end
-        arguement_counter += 1
       end
       return data_type, column_name
     end
 
     def validate argv
-      validate_flag = false
-      arguement_datatype = []
-      min_arguement_length = 3
-      data_type = ["integer","float","boolean","string","text"]
-      argv.each_with_index do |arguement,index|
-        if index >= min_arguement_length
-          arguement_datatype = arguement.split(':')
-          if data_type.include?(arguement_datatype[0])
-            validate_flag = true
-          else
-            validate_flag = true
-            return validate_flag
-          end
-        end
-        if argv.length == min_arguement_length
-          validate_flag = true
+      validate_flag = true
+      argument_datatype = []
+      if argv.length > MIN_ARGUMENT_LENGTH
+        data_type_column_name_array = argv.drop(3)
+        argument_data_type = split_argument data_type_column_name_array
+        unless DATA_TYPE.any?{ |x| argument_data_type.include?(x)}
+          validate_flag = false
         end
       end
       validate_flag
     end
 
-    def set_column data_type, column_name, model_class_name,argv
+    def split_argument data_type_column_name_array
+      argument_data_type = data_type_column_name_array.collect{ |element| element.split(":")[0]}
+      argument_data_type
+    end
+
+    def set_column data_type, column_name, model_class_name,file_name
       loop_counter = 0
-      file_name = method_name argv
       write_file = File.open("app/models/#{file_name}.rb","a+")
       write_file.each_line do |line|
         if line.scan"class #{model_class_name}"
@@ -94,11 +97,11 @@ class Model
       write_file.close
     end
 
-    def method_name argv
+    def get_file_name argv
       create_model_flag = 0
       if argv.first.eql?"g"
         if argv[2].nil?
-          puts "Model Name is not defined"
+          abort "Model Name is not defined"
         elsif argv[2].downcase.eql?"model"
           abort "Sorry..Keywork Model cant be used in place of model_name"
         else
