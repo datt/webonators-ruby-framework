12c477b9f21bd40e961a23addcf1ae02bfa341ab
Palash Kulkarni <palash.kulkarni@weboniselab.com>
2014-09-08 15:56:47 +0530

    changes in generator

--- webit/lib/webit/request.rb
+++ webit/lib/webit/request.rb
@@ -1,35 +1,20 @@
 require 'rack'
-require 'rack/respond_to'
 require File.expand_path('../dispatcher', __FILE__)
-class Request
-  include Rack::RespondTo #mixes in #respond_to
+require File.expand_path('../response', __FILE__)
 
+class Request
+
+  # define -> call(). This method is called automatically whenever any request is hit on browser.
+  # Takes browser's env hash as arguement and returns response back to browser.
   def self.call(env)
     request = Rack::Request.new(env)
     path = request.path_info
-    params = request.params
-    unless request.post?
-      content = Dispatcher.dispatch path
-    else
-      content = Dispatcher.dispatch path, params
+    params = nil
+    if request.post?
+      params = request.params
     end
-    get_response content
+    content = Dispatcher.dispatch *[path, params].compact
+    Response.response content
   end
 
-  def self.get_response content
-    Rack::RespondTo.media_types = ['text/html']
-    redirect_url = nil
-    body = respond_to do |format|
-      if content.class == String
-        format.html { content }
-      else
-        redirect_url = content[:url]
-      end
-    end
-    unless redirect_url
-      [200, {'Content-Type' => Rack::RespondTo.selected_media_type}, [body]]
-    else
-      [302, {'Location' => redirect_url}, []]
-    end
-  end
 end
